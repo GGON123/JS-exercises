42497e2be35cb6bc6c05f39ec1030a3bddc10715
GGON123 <jin0405@naver.com>
2024-08-13 13:12:11 +0900

    destructuring.js add contents

--- modern/destructuring.js
+++ modern/destructuring.js
@@ -8,13 +8,22 @@ console.log(coupon);
 // 객체 구조 분해
 const macbookPro = {
   title: 'MBP 16',
-  price: 400000
+  price: 400000,
+  memory: '16GB',
+  storage: '1TB'
 };
-const {title, price} = macbookPro;
-console.log(title, price);
-console.log(title);
-console.log(price);
+// const {title, price} = macbookPro;
+// console.log(title, price);
+// console.log(title);
+// console.log(price);
 
 const members2 = ['Kang', 'Myoung', undefined, 'Gon', 'GGON'];
 const [mac, pad, airpod = 'Jack', ...coupons] = members2;
 console.log(mac, pad, airpod, coupons);
+
+const {title, price, color = 'silver', ...rest} = macbookPro;
+console.log(title);
+console.log(price);
+console.log(color);
+console.log(rest);
+console.log(rest.memory);
